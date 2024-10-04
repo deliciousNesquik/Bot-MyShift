25abf7434eebcdf3217546e749fd63e9832cf7a6
nesquik <99757380+deliciousNesquik@users.noreply.github.com>
2024-10-04 12:40:17 +0500

    Delete app directory

--- app/states/RegisterEmployee.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class RegisterEmployee(StatesGroup):
-    id = State()
-    full_name = State()
-    age = State()
-    phone = State()
-    bank = State()
